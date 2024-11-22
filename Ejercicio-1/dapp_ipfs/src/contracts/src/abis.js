bcbfad08771496eda7fa9569461b6b31df0e52ed
cristian.vega <cristian.vega@aimen.es>
2024-11-22 22:44:17 +0100

    Initial commit

---
+++ Ejercicio-1/dapp_ipfs/src/contracts/src/abis.js
@@ -0,0 +1,12 @@
+//import erc20Abi from "./abis/erc20.json";
+//import ownableAbi from "./abis/ownable.json";
+import ipfsStorage from "./abis/IpfsStorage.json";
+import decentralizedNFT from "./abis/decentralizedNFT.json"
+const abis = {
+//  erc20: erc20Abi,
+//  ownable: ownableAbi,
+  ipfs: ipfsStorage,
+  decentralizedNFT: decentralizedNFT,
+};
+
+export default abis;
---
+++ Ejercicio-1/dapp_ipfs/src/contracts/src/addresses.js
@@ -0,0 +1,6 @@
+const addresses = {
+  ipfs: "0xb3c97c79bb061d09c7a3a8e72758fb4191be9a76",
+  decentralizedNFT: "0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8"
+};
+
+export default addresses;
